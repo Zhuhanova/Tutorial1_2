75f29aa4e40b183fade76b5256d068cf1e04e65c
Alisa Zhuhanova <s30269@pjwstk.edu.pl>
2025-03-14 09:44:04 +0100

    Task1

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main{
+
+public static void main(String[] args) {
+
+    int[] arr = {1, 2, 3, 4, 5, 6};
+    int[] arr1 = filterEvenIndexOddValue(arr);
+
+    for(int i=0; i<arr1.length; i++){
+        if(arr1[i]!=0) System.out.print(arr1[i] + " ");
+    }
+}
+public static int[] filterEvenIndexOddValue(int[] arr){
+
+    int[] arr2 = new int[arr.length];
+    for(int i = 0; i<arr.length; i++){
+        if(i%2==0&&arr[i]%2!=0)
+            arr2[i] = arr[i];
+    }
+    return arr2;
+}
+        }
