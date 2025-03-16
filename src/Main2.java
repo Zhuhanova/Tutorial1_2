cf7f48c301a622f4bde1ddd6b8390fb147917296
Alisa <s30269@pjwstk.edu.pl>
2025-03-16 16:25:33 +0100

    Updated Main2.java

--- src/Main2.java
+++ src/Main2.java
@@ -1,2 +1,34 @@
 public class Main2 {
+    public static void main(String[] args) {
+        
+        int[] arr = {3, 3, 3, 2, 2};
+        System.out.println(findDominant(arr));
+    }
+
+    public static int findDominant(int[] arr){
+
+        int count = 0;
+        int num = -1;
+        int half;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if(arr[i]==arr[j]) count++;
+            }
+
+            if(arr.length%2==0){
+                half = arr.length/2;
+            }else{
+                half = arr.length/2+1;
+            }
+
+            if(count>= half){
+                num = arr[i];
+                return num;
+            }else{
+                count=0;
+            }
+        }
+        return num;
+    }
 }
